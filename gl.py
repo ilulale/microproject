e92e2626f03a247a97d3ace043e382395e9b8b89
ilulale <56584872+ilulale@users.noreply.github.com>
2020-03-19 17:45:39 +0530

    Add files via upload

--- gl.py
+++ gl.py
@@ -1,17 +1,33 @@
 from googlesearch import search
 
+def cleaner(res):
+    clean=[]
+    file=open("trustlist.txt","r")
+    trustlist=[l.strip() for l in file.readlines()]
+    print(trustlist)
+    file.close()
+    for x in range(len(trustlist)):
+        for i in range(len(res)):
+            if res[i].find(trustlist[x])!=-1:
+                clean.append(res[i])
+            else:
+                continue
+    return clean
+
 ip=input("What would you like to search for? ")
 
-max_search=10
+max_search=1000
 res = []
 clean = []
+user_agnt = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 
-for url in search(ip, stop=max_search):
+for url in search(ip, stop=max_search,user_agent= user_agnt,country='india'):
      res.append(url)
 
-for i in range(max_search):
-    if res[i].find("gov")!=-1:
-        clean.append(res[i])
-    else:
-        print("False "+res[i])
-print(clean)
+clean=cleaner(res)
+tot=0
+for  i in range(len(clean)):
+    print(clean[i])
+    tot=tot+1
+
+print(str(tot)+"Trusted sources")
